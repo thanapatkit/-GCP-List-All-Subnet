ad03a458d9f8eade330ccd98dfb02469729ca9ef
Thanapat_k <thanapat_k@mcloudy.net>
2021-07-30 04:22:10 +0000

    Modify script, Add Allocate IP Range

--- src/list_subnet.sh
+++ src/list_subnet.sh
@@ -2,7 +2,7 @@
 
 rm -f $PWD/csv_vpc_subnet.csv
 
-echo Project Name,VPC Name,Subnet Name,Subnet IP,Secondary name,Secondary IP > $PWD/csv_vpc_subnet.csv
+echo Project Name,VPC Name,Subnet Name,Subnet IP,Secondary name,Secondary IP,Remark > $PWD/csv_vpc_subnet.csv
 
 while IFS= read -r line
 do
@@ -19,5 +19,23 @@ do
             secondary_ip=$(gcloud compute networks subnets list --project=$line --network=$vpcnetwork --filter="name=$subnet" --format="value(secondaryIpRanges.ipCidrRange)")
             echo $project_id,$vpc_name,$subnet_name,$subnet_ip,$secondary_name,$secondary_ip >> $PWD/csv_vpc_subnet.csv
         done
+    done
+done < $PWD/src/list_projects_id
+
+while IFS= read -r line2
+do
+    for allocateip in $(gcloud compute addresses list --global --filter="purpose=VPC_PEERING" --project=$line2 --format='value(name)')
+	do
+        printf "$(gcloud compute addresses list --global --filter=$allocateip --project=$line2 --format='value(network.scope(),name)')\n"
+        project_id=$(gcloud projects describe $line2 --format="value(name)")
+        vpc_name=$(gcloud compute addresses list --global --filter=$allocateip --project=$line2 --format='value(network.scope())')
+        subnet_name=$(gcloud compute addresses list --global --filter=$allocateip --project=$line2 --format='value(name)')
+        address=$(gcloud compute addresses list --global --filter=$allocateip --project=$line2 --format='value(address)')
+        prefixLength=$(gcloud compute addresses list --global --filter=$allocateip --project=$line2 --format='value(prefixLength)')
+        subnet_ip="${address}/${prefixLength}"
+        secondary_name=" "
+        secondary_ip=" "
+        remark="Allocate IP Range"
+        echo $project_id,$vpc_name,$subnet_name,$subnet_ip,$secondary_name,$secondary_ip,$remark >> $PWD/csv_vpc_subnet.csv
     done
 done < $PWD/src/list_projects_id
